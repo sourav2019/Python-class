699d19edbd1e308889d2b00082fc2ebb1dbefab3
sourav2019 <souravsaha113@yahoo.com>
2025-05-25 13:29:47 +0530

    First commit

---
+++ Class/Day-one.py
@@ -0,0 +1,54 @@
+# printing
+# comments
+# variable
+# dataType (int,fl,str,bool,list,dic)
+# input
+# git
+# ifelse?
+
+value = 10;
+print(value)
+value = 2;
+print(value)
+value="Udi"
+print(value)
+value=True
+
+print(value);
+
+# ujgehkeuilyeri
+name = "Sourav!"
+day = "This is sunday"
+
+# print("Hello, good morning", name, day)
+
+# day = "this is monday";
+# print("Hello, good morning", name, day)
+
+day=True
+
+print("Hello, good morning", name, day)
+# dictionary datatype/object
+user = {
+    name:"Udi",
+    address:{
+        city:"kolkata",
+        country: "IND",
+        code:700047
+    },
+    age:24,
+    adult: True,
+    hobby:["Cricket", "movie", "sleeping"],
+    result: [20, 12,],
+   
+}
+# DATA TYPES
+# integer 1
+# float 1.97475897670
+# boolean True False
+# string
+# Array/list
+# dictionary
+
+
+
